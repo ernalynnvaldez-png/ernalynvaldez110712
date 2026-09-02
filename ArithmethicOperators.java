03b7ab7639154f491ebece92d73103684c72cf98
ernalynnvaldez-png <ernalyn.n.valdez@isu.edu.ph>
2026-09-02 10:13:24 -0500

    Activity 2.14 ArithmethicOperators.java

---
+++ ArithmethicOperators.java
@@ -0,0 +1,46 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        // Declare variables
+        int i = 37;
+        int j = 42;
+        double x = 27.475;
+        double y = 7.22;
+
+        // Display variable values
+        System.out.println("Variable values...");
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        // Adding
+        System.out.println("Adding...");
+        System.out.println("i + j = " + (i + j));
+        System.out.println("x + y = " + (x + y));
+
+        // Subtracting
+        System.out.println("Subtracting...");
+        System.out.println("i - j = " + (i - j));
+        System.out.println("x - y = " + (x - y));
+
+        // Multiplying
+        System.out.println("Multiplying...");
+        System.out.println("i * j = " + (i * j));
+        System.out.println("x * y = " + (x * y));
+
+        // Dividing
+        System.out.println("Dividing...");
+        System.out.println("i / j = " + (i / j));
+        System.out.println("x / y = " + (x / y));
+
+        // Computing the remainder
+        System.out.println("Computing the remainder...");
+        System.out.println("i % j = " + (i % j));
+        System.out.println("x % y = " + (x % y));
+
+        // Mixing types
+        System.out.println("Mixing types...");
+        System.out.println("j + y = " + (j + y));
+        System.out.println("i * x = " + (i * x));
+    }
