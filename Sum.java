9db5029513bb9198a3024a2baa520c562a22e59d
ernalynnvaldez-png <ernalyn.n.valdez@isu.edu.ph>
2026-09-02 10:10:41 -0500

    Activity 2.13 Sum.java

---
+++ Sum.java
@@ -0,0 +1,18 @@
+public class Sum {
+    public static void main(String[] args) {
+
+        // Two integers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the output
+        System.out.println("Enter first integer: " + num1);
+        System.out.println("Enter second integer: " + num2);
+        System.out.println("The sum of the two integers is: " + sum);
+    }
+}
+Compose
+Write to Shasha Fernandez
