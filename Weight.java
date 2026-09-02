1816db83962c1bb73ad0873d437f58c1565bde55
ernalynnvaldez-png <ernalyn.n.valdez@isu.edu.ph>
2026-09-02 09:48:03 -0500

    Ativity2.8 Weight.java

---
+++ Weight.java
@@ -0,0 +1,12 @@
+public class Weight {
+
+    public static void main(String[] args) {
+
+        double pounds = 10;
+        double kilograms;
+
+        kilograms = pounds * 0.454;
+
+        System.out.println("Weight in pounds: " + pounds);
+        System.out.println("Weight in kilograms: " + kilograms);
+    }
