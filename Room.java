30d6432aaf499a43fa030bd544d04017cec2458f
ernalynnvaldez-png <ernalyn.n.valdez@isu.edu.ph>
2026-09-02 09:51:31 -0500

    Ativity2.9 Room.java

---
+++ Room.java
@@ -0,0 +1,10 @@
+public class Room {
+    public static void main(String[] args) {
+        int length = 20;
+        int width = 15;
+
+        int floorSpace = length * width;
+
+        System.out.println("The floor space is " + floorSpace + " square feet.");
+    }
+}
