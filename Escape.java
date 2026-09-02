990c438d16bdb08169fd7f8bb0455b6e0c435649
ernalynnvaldez-png <ernalyn.n.valdez@isu.edu.ph>
2026-09-02 10:18:59 -0500

    Activity 2.15 Escape.java

---
+++ Escape.java
@@ -0,0 +1,11 @@
+public class Escape {
+    public static void main(String[] args) {
+
+        // Display the column headings
+        System.out.println("First Name\tLast Name\tStudent Number\tPhone Number");
+
+        // Display student information
+        System.out.println("Ernalyn\t\tValdez\t[26-172]\t[09461840403]");
+    }
+}
+Compose
