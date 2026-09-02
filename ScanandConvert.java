30024aea503f31767d25c1d41d9e999e2815e974
ernalynnvaldez-png <ernalyn.n.valdez@isu.edu.ph>
2026-09-02 10:27:11 -0500

    Activity 2.18 ScanandConvert.java

---
+++ ScanandConvert.java
@@ -0,0 +1,19 @@
+public class ScanAndConvert {
+    public static void main(String[] args) {
+
+        // Grades of the three subjects
+        double javaGrade = 85;
+        double cppGrade = 90;
+        double csharpGrade = 95;
+
+        // Calculate the average
+        double average = (javaGrade + cppGrade + csharpGrade) / 3;
+
+        // Display the grades and average
+        System.out.println("Java Grade: " + javaGrade);
+        System.out.println("C++ Grade: " + cppGrade);
+        System.out.println("C# Grade: " + csharpGrade);
+        System.out.println("Total Average: " + average);
+    }
+}
+Compose
